4145d59640834cee862a2af8150f600859b33b57
himanshubora7 <himanshubora972@gmail.com>
2026-07-09 18:38:21 +0530

    working iteration 1

--- intel-ui/src/App.jsx
+++ intel-ui/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LogoDemo from "./pages/LogoDemo";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/logo-demo" element={<LogoDemo />} />
+      <Route path="/detail/:postId" element={<Detail />} />
     </Routes>
   );
 }
